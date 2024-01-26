5b21ab1c843d1502476ed9ec8592ac34a326f99b
Chris Awoke <uchechristian27@gmail.com>
2024-01-26 20:40:58 +0100

    Update sanity.cli.js

--- backend/sanity.cli.js
+++ backend/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'gu7h7ncn',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
