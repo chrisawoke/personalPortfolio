6249226f0bb5e744117b45f5b808304950a56322
Chris Awoke <uchechristian27@gmail.com>
2024-01-26 20:39:46 +0100

    Update index.js

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -8,4 +8,13 @@ import skills from './skills'
 import workExperience from './workExperience'
 import contact from './contact'
 
-export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperience, experiences, contact]
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+]
