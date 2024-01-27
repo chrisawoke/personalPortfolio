1fe58fdab83a46e7fedcefa6a5af5696c5726084
Chris Awoke <uchechristian27@gmail.com>
2024-01-27 21:06:21 +0100

    Update index.js

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -1,8 +1,5 @@
-import About from "./About/About";
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import Skills from "./Skills/Skills";
-import Testimonial from "./Testimonial/Testimonial";
-import Work from "./Work/Work";
-
-export { About, Footer, Header, Skills, Testimonial, Work };
+export { default as Footer } from "./Footer/Footer";
+export { default as Testimonial } from "./Testimonial/Testimonial";
+export { default as Work } from "./Work/Work";
+export { default as Skills } from "./Skills/Skills";
+export { default as Header } from "./Header/Header";
